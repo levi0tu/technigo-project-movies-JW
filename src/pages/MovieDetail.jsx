1c2f1139dad99d0ccb51f5f9582f1800a53e116c
Jeanette Wiktorsson <leiia@hotmail.com>
2026-02-26 11:34:21 +0100

    Detail page styled

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -6,7 +6,7 @@ import { useParams } from "react-router-dom";
 const MovieDetail = () => {
     const { id } = useParams();
     const apiKey = import.meta.env.VITE_TMDB_API_KEY;
-    const [movie, setMovie] = useState([]);
+    const [movie, setMovie] = useState([null]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
 
@@ -27,13 +27,13 @@ const MovieDetail = () => {
                 setError("Something went wrong");
                 setLoading(false);
             });
-    }, [apiKey]);
+    }, [id, apiKey]);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>{error}</p>;
     if (!movie) return null;
 
-    const backdropUrl = movie.backdrop_path ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
+    const backdropUrl = movie.backdrop_path ? `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`
         : "";
     const posterUrl = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
         : "https://via.placeholder.com/500x750?text=No+Image";
@@ -41,11 +41,22 @@ const MovieDetail = () => {
     return (
         <main className="movie-detail-page">
             <section className="hero" style={{ backgroundImage: `url(${backdropUrl})` }}>
-                <img src={posterUrl} alt={movie.title} />
-                <div>
-                    <h1>{movie.title}</h1>
-                    <p>{year} • {movie.runtime} min </p>
-                </div></section></main>);
+                <div className="hero-darken" />
+                <div className="hero-fade-bottom" />
+
+                <a href="/" className="back-link">
+                    <span className="back-circle">
+                        <span className="back-icon">◀</span></span> Movies</a>
+
+                <div className="hero-content">
+                    <img className="detail-poster" src={posterUrl} alt={movie.title} />
+
+                    <div className="detail-info">
+                        <h1>{movie.title}</h1>
+                        <span className="rating">
+                            <span className="star">★</span> {movie.vote_average?.toFixed(1)}</span>
+                        <p className="movie-text">{movie.overview}</p>
+                    </div></div></section></main>);
 };
 
 export default MovieDetail;
